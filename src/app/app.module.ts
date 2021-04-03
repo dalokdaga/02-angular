36d8b1b57242841b589f9972907edda5e1aa6b3f
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-04-03 14:59:35 -0600

    video 9 terminado routerlink y routerlinkactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//rutas
 import { APP_ROUTING } from './app.routes';
 
-
+//componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
