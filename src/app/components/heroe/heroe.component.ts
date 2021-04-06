acbd9c4f27032439832e4e53a8c659ad562cd685
--global <dgarcia@grupomabarak.com.mx>
2021-04-06 10:47:32 -0500

    avance hasta el video 18 - curso 4

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { HeroesService } from "../../servicios/heroes.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -21,5 +22,8 @@ export class HeroeComponent implements OnInit {
       console.log(this.heroe)
     });
   }
-
+  ngOnInit() {
+    //this.heroes = this._heroesService.getHeroes();
+    //console.log( this.heroes );
+  }
 }
